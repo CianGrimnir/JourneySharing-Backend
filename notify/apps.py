56b8e16a92a6c10add724f7204ad7c24453ef3e4
Catess <bhattaa1@tcd.ie>
2022-01-31 10:23:02 +0000

    initial notify signal code

--- notify/apps.py
+++ notify/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class NotifyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notify'
+    verbose_name = ''
+
+    def ready(self):
+        import notify.signals
--- notify/views.py
+++ notify/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from .models import Notification
+
+@receiver(post_save, sender=Comment)
+def auto_create_notification(sender, instance, created, **kwargs):
+    if created:
+        # instance holds the new comment (reply), but you also have to fetch
+        # original comment and the user who created it
+        parent_comment = instance.parent_comment
+        parent_user = parent_comment.user            
+        Notification.objects.create(user=parent_user,
+                                    type="reply_created")
