7f4cfd242191789cbb0099178c2675ca82935713
Rakesh N <ranair@tcd.ie>
2022-02-01 04:33:27 +0000

    JS-7 - Added a new field - age for user model.

--- register/models.py
+++ register/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import EmailValidator, DecimalValidator
+from django.core.validators import EmailValidator
 
 
 # Create your models here.
@@ -8,5 +8,6 @@ class User(models.Model):
     user_id = models.CharField(max_length=20)
     first_name = models.CharField(max_length=15)
     last_name = models.CharField(max_length=15)
+    age = models.IntegerField()
     email = models.CharField(max_length=30, validators=EmailValidator)
-    phone_number = models.DecimalField(max_length=10, validators=DecimalValidator(10))
+    phone_number = models.BigIntegerField(max_length=10)
