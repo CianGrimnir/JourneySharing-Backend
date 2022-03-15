bdc695a7bc6a3cab29ebc9ed42a990b00768aa7f
Rakesh Nair <ranair@tcd.ie>
2022-03-15 00:18:00 +0000

    JS-24 - Added more test cases for user module.

--- user/tests.py
+++ user/tests.py
@@ -1,7 +1,6 @@
 from decimal import Decimal
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
-
 from user.views import get_user_profile
 from rest_framework.status import (
     HTTP_200_OK,
@@ -16,6 +15,8 @@ services.logger.setLevel(logging.ERROR)
 
 class GetUserProfileTest(TestCase):
     proper_login_body = {'email_address': 'asd@dsa.com', 'token': 'testmocktoken'}
+    mismatch_email_login_body = {'email_address': 'asd@dsa.co', 'token': 'testmocktoken'}
+    missing_token_login_body = {'email_address': 'asd@dsa.com'}
     wrong_user_email_body = {'email_address': 'asd@dsa.co'}
     proper_mock_api_call = Service.build_ok_response(**{
         'item': {'password': 'asddsa', 'user_id': 'asdffsda', 'last_name': 'dsa123', 'first_name': 'asd', 'phone_number': Decimal('123544586765'), 'email': 'asd@dsa.com',
@@ -40,5 +41,25 @@ class GetUserProfileTest(TestCase):
         mock_get_item_from_table.return_value = mock_api_call
         factory = APIRequestFactory()
         request = factory.post('/user/profile', self.wrong_user_email_body)
+        response = get_user_profile(request)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
+
+    @patch('services.dynamodb.DynamoDbService.get_item_from_table')
+    @patch('services.redis.Redis.get_values')
+    def test_missing_token(self, mock_redis_get_values, mock_get_item_from_table, mock_api_call=wrong_user_profile_mock_api_call, mock_token=mock_token_api_call):
+        mock_redis_get_values.return_value = mock_token
+        mock_get_item_from_table.return_value = mock_api_call
+        factory = APIRequestFactory()
+        request = factory.post('/user/profile', self.missing_token_login_body)
+        response = get_user_profile(request)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
+
+    @patch('services.dynamodb.DynamoDbService.get_item_from_table')
+    @patch('services.redis.Redis.get_values')
+    def test_mismatch_email(self, mock_redis_get_values, mock_get_item_from_table, mock_api_call=wrong_user_profile_mock_api_call, mock_token=mock_token_api_call):
+        mock_redis_get_values.return_value = mock_token
+        mock_get_item_from_table.return_value = mock_api_call
+        factory = APIRequestFactory()
+        request = factory.post('/user/profile', self.mismatch_email_login_body)
         response = get_user_profile(request)
         self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
